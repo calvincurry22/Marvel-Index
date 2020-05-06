416117c61c10efd2f535bfbe44821a6c26491793
calvincurry22 <calvincurry22@gmail.com>
2020-05-05 22:15:27 -0500

    Added styling for the login page

--- src/components/auth/Auth.js
+++ src/components/auth/Auth.js
@@ -1,16 +1,46 @@
-import React from "react"
+import React, { useState } from "react"
 import Login from "./Login"
 import Register from "./Register"
 import "./Auth.css"
+import { Button } from "@material-ui/core"
+import { ModalHeader, Modal, ModalBody } from "reactstrap"
 
 
 export default ({toggle}) => {
+    const [signInModal, setSignInModal] = useState(false)
+    const toggleSignIn = () => setSignInModal(!signInModal)
+    const [registerModal, setRegisterModal] = useState(false)
+    const registerToggle = () => setRegisterModal(!registerModal)
     return (
         <>
-            <h1 className="welcome">Marvel Index</h1>
             <div className="authContainer">
-                <Login toggle={toggle} />
-                <Register toggle={toggle} />
+                <button className="signInButton login" onClick={evt => {
+                    toggleSignIn()
+                }}>Sign In</button>
+                <button className="signUpButton login" onClick={evt => {
+                    registerToggle()
+                }}>Sign Up</button>
+            </div>
+                
+                
+            <div className="authModalContainer">
+            <Modal isOpen={signInModal} toggle={toggleSignIn}>
+                <ModalHeader toggle={toggleSignIn}>
+                    <h4>Login</h4>
+                </ModalHeader>
+                <ModalBody>
+                    <Login toggle={toggle} />
+                </ModalBody>
+            </Modal>
+            
+            <Modal isopen={registerModal} toggle={registerToggle}>
+                <ModalHeader toggle={registerToggle}>
+                    <h4>Sign Up</h4>
+                </ModalHeader>
+                <ModalBody>
+                    <Register toggle={toggle} />
+                </ModalBody>
+            </Modal>
             </div>
         </>
     )
--- src/components/sideNav/SideNav.js
+++ src/components/sideNav/SideNav.js
@@ -1,17 +1,40 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import "./SideNav.css"
-
+import { List, ListItem, ListItemIcon, ListItemText, Icon, SvgIcon } from "@material-ui/core"
 
 
 export default () => (
     <div className="sideNavContainer">
         <nav className="sideNav">
-            <Link to="/">My List</Link>
-            <Link to="/readComics">Read Comics</Link>
-            <Link to="/comicsExplorer">Explore Comics</Link>
-            <Link to="/forum">Forum</Link>
-            <Link to="/account">Account</Link>
+            <List>
+                <ListItem>
+                    <ListItemText>
+                        <Link to="/">My List</Link>
+                    </ListItemText>
+                </ListItem>
+                <ListItem>
+                    <ListItemText>
+                        <Link to="/readComics">Read Comics</Link>
+                    </ListItemText>
+                </ListItem>
+                <ListItem>
+                    <ListItemText>
+                        <Link to="/comicsExplorer">Explore Comics</Link>
+                    </ListItemText>
+                </ListItem>
+                <ListItem>
+                    <ListItemText>
+                        <Link to="/forum">Forum</Link>
+                    </ListItemText>
+                </ListItem>
+                <ListItem>
+                    <ListItemText>
+                        <Link to="/account">Account</Link>
+                    </ListItemText>
+                </ListItem>
+            </List>
         </nav>
     </div>
 )
+
